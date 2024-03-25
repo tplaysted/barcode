cd62c117f2a14f1c828a70103d995174564f9968
tplay <t.playsted@gmail.com>
2024-03-25 15:34:05 +1100

    Hello world

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <fstream>
+
+const int THRESHOLD = 127;
+const int CAMERA_PORT = 0;
+const double pi = 3.14159265358979323846;
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
